33b2ae683ddad3f7eafeaf2c66bfdd64deddf730
Bishawjit Das <bishawjit387@gmail.com>
2020-04-05 15:35:48 +0600

    Change config file

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <p>This is the about page</p>
+            <p><a href="/">Index page</a></p>
+        </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Index() {
+    return (
+        <div>
+            <p>Hello Next.js</p>
+            <p><a href="/about">About page</a></p>
+        </div>
+    );
+}
